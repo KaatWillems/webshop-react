2e321e78f1e52a2cc86e2e9f5a076bd040666ab7
Kaat Willems <97091300+KaatWillems@users.noreply.github.com>
2022-07-06 10:57:43 +0200

    fixed navbar homepage. content is added. but still needs to be properly positioned

--- src/components/Homepage/Headerhome.js
+++ src/components/Homepage/Headerhome.js
@@ -8,16 +8,22 @@ function Headerhome() {
     <div className='headerhome-cont'>
       <div className="banner">
             <p className="banner-text">Free shipping in Belgium</p>
-
         </div>
         <Logo className="logo logo-home"/>
-      <div className='nav-insta-cont'>
-            
-      <div className='instabubble-cont-homepage'>
-        <Instabubble />
-        </div>
+         <div className='nav-insta-cont'>            
+            <div className='instabubble-cont-homepage'>
+              <Instabubble />
+            </div>
+
             <Navbar />
-      </div>
+
+            <div className='home-headericons'>
+                <div className='cart'></div>
+                <div className='account'></div>
+                 <div className='search'></div>
+            </div>    
+        </div>
+     
     </div>
   )
 }
